b2219e96b4f583c1399fbf63c0e41873392b676e
Brendan Grubb and Benjamin T. Seaver <brendan@>
2017-02-08 10:50:58 -0800

    create initial Silex files

---
+++ app/app.php
@@ -0,0 +1,14 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex\Application();
+
+    $app->get('/', function() {
+        return "<p>Hello Ben!</p>";
+
+    });
+
+    return $app;
+
+?>

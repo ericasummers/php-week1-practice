835bc6c042b0bb8e9ee451ca354d690f4b309b0b
Brendan Grubb and Benjamin T. Seaver <brendan@>
2017-02-08 16:35:53 -0800

    refactor delete_all route with Twig

--- app/app.php
+++ app/app.php
@@ -16,61 +16,10 @@
         array('twig.path' => __DIR__.'/../views')
     );
 
-    $app->get('/', function() {
+    $app->get('/', function() use ($app) {
         $jobs = Job::getAll();
 
-        $output = '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
-                <title>Job History</title>
-            </head>
-            <body>
-                <div class="container">';
-
-        if ($jobs) {
-            $output .= "<h2>Jobs Entered So Far</h2>";
-        }
-
-        foreach ($jobs as $job) {
-            $output .= "<h3>" . $job->getWhenEmployed();
-            $output .= " / " . $job->getJobTitle();
-            $output .= " / " . $job->getEmployer() . "</h3>";
-        }
-
-        if ($jobs) {
-            $output .= "<br>";
-        }
-
-        $output .=
-                '<h2>Please enter a job to add to your employment history:</h2>
-                    <form action="/added_job" method="post">
-                        <div class="form-group">
-                            <label for="when_employed">When were you employed?</label>
-                            <input id="when_employed" class="form-control" name="when_employed" type="text">
-                        </div>
-                        <div class="form-group">
-                            <label for="job_title">Title</label>
-                            <input id="job_title" class="form-control" name="job_title" type="text">
-                        </div>
-                        <div class="form-group">
-                            <label for="employer">Who was your employer?</label>
-                            <input id="employer" class="form-control" name="employer" type="text">
-                        </div>
-                        <button class="btn" type="submit">Add a Job!</button>
-                    </form>
-                    <br>
-                    <form action="/delete_all" method="post">
-                        <button class="btn btn-warning" type="submit">Delete All Jobs</button>
-                    </form>
-                </div>
-            </body>
-        </html>
-
-        ';
-
-        return $output;
+        return $app['twig']->render('view_all.html.twig', array('jobs' => $jobs));
     });
 
 
